f719b7bef47d33cf6b60c3b58c5033b782828e70
Sh1417 <sharathhari17@gmail.com>
2024-11-18 20:20:49 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -5,16 +5,73 @@ const User = require("./models/user.js")
 
 app.use(express.json());
 
+//SIGNUP-API
 app.post("/signUp", async (req,res)=>{
     const user = new User(req.body)
     try{
         await user.save();
         res.send("User saved successfully")
     }catch(err){
-        res.status(400).send("It's a bad request")
+        res.status(400).send("It's a bad request"+err.message)
     }
 })
 
+//Find by email id 
+app.get("/user",async (req,res)=>{
+    const userEmail = req.body.emailId; 
+
+    try{
+        const users = await User.find({emailId: userEmail})
+        if(users.length === 0){
+            res.send("User not found")
+        }else{
+            res.send(users)
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+   
+})
+
+//FEED API - Get /feed
+app.get("/feed", async(req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users)
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+//DELETE BY ID 
+app.delete("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+//UPDATE DATA OF THE USER
+app.patch("/update", async(req,res)=>{
+    try{
+        const data = req.body;
+        const userId = req.body.userId;
+        const ALLOWED_UPDATES = ["userId","skills","password","photoUrl","about"];
+        const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k))
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed")
+        }
+        await User.findByIdAndUpdate(userId, data , {
+            runValidators: true,
+        });
+        res.send("User updated successfully")
+    }catch(err){
+        res.status(400).send("UPDATE FAILED" +  err.message)
+    }
+})
 
 connectDb()
 .then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,51 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type: String 
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
+
     },
     lastName:{
         type: String 
     },
     emailId:{
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim:true,
     },
     password:{
-        type: String
+        type: String,
+        required: true,
     },
     age:{
-        type: String
+        type: String,
+        min:18,
+
     },
     gender:{
-        type: String
+        type: String,
+        validate(value){
+            if(!['male','female','others'].includes(value)){
+                throw new Error("Gender is not valid")
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.kindpng.com/picc/m/252-2524695_dummy-profile-image-jpg-hd-png-download.png",
+    },
+    about:{
+        type: String,
+        default: "This is a default about of the user"
+    },
+    skills:{
+        type: [String],
     }
-})
+},{timestamps: true})
 
 module.exports = mongoose.model("User", userSchema);
 
